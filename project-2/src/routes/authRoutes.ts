8a975a79f59f68f8a920aa8eba375775d1f40097
Sougata <sougatadas9874@gmail.com>
2025-01-31 11:44:51 +0530

    add registration logic.

--- project-2/src/routes/authRoutes.ts
+++ project-2/src/routes/authRoutes.ts
@@ -4,9 +4,47 @@ import bcrypt from "bcryptjs";
 
 import db from "../db.ts";
 
+const SECRET = process.env.JWT_SECRET || "backup_secret_key";
+
 const router = Router();
 
-router.post("/register", (req, res) => {});
+//signup route
+router.post("/register", (req, res) => {
+  const { username, password } = req.body;
+
+  //encrypt the password
+  const hashedPassword = bcrypt.hashSync(password, 8);
+
+  //save credentials to db
+  try {
+    const insertUser = db.prepare(`
+            INSERT INTO user (username, password)
+            VALUES (?, ?)
+        `);
+    const result = insertUser.run(username, hashedPassword);
+
+    // adding a default todo
+    const defaultTodo = `Hello! Add your first todo`;
+    const insertTodo = db.prepare(`
+        INSERT INTO todo (user_id, task)
+        VALUES (?, ?)
+        `);
+    insertTodo.run(result.lastInsertRowid, defaultTodo);
+
+    //create a token
+    const token = jwt.sign({ id: result.lastInsertRowid }, SECRET, {
+      expiresIn: "24h",
+    });
+
+    //this token is returned as a response to the client, which is then stored in a cookie/local-storage to check for a valid user.
+
+    //checking part is in middleware
+    res.json({ token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Internal server error");
+  }
+});
 
 router.post("/login", (req, res) => {});
 
