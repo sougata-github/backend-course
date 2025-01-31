d66a7a90ba0b43f485459d0e7c9e9399abe20e79
Sougata <sougatadas9874@gmail.com>
2025-01-31 14:24:26 +0530

    add crud operations and finish project-2.

--- project-2/src/routes/todoRoutes.ts
+++ project-2/src/routes/todoRoutes.ts
@@ -13,12 +13,44 @@ router.get("/", (req: any, res) => {
 });
 
 //creating a todo
-router.post("/", (req, res) => {});
+router.post("/", (req: any, res) => {
+  const { task } = req.body;
+  const insertTodo = db.prepare(`
+      INSERT INTO todo (user_id, task)
+      VALUES (?, ?)
+    `);
+  const result = insertTodo.run(req.userId, task);
+
+  res.json({ id: result.lastInsertRowid, task, completed: 0 });
+});
 
 //updating a todo -> dynamic params
-router.put("/:id", (req, res) => {});
+router.put("/:id", (req, res) => {
+  const { completed } = req.body;
+  const { id } = req.params; //coming from the url for example: /todos/1 or /todos/2
+
+  const updatedTodo = db.prepare(`
+    UPDATE todo SET completed = ? WHERE id = ?
+    `);
+  updatedTodo.run(completed, id);
+
+  res.json({
+    message: "Todo completed",
+  });
+});
 
 //deleting a todo -> dynamic params
-router.delete("/:id", (req, res) => {});
+router.delete("/:id", (req: any, res) => {
+  const { id } = req.params;
+
+  const deletedTodo = db.prepare(`
+    DELETE FROM todo WHERE id = ? AND user_id = ?
+    `);
+  deletedTodo.run(id, req.userId);
+
+  res.json({
+    message: "Todo deleted",
+  });
+});
 
 export default router;
